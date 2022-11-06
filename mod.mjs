27bde124876c2f827e52f0a972c9a2b9873d6356
od <y_ohde@odproductions.jp>
2022-11-07 01:16:12 +0900

    Update mod.mjs

--- mod.mjs
+++ mod.mjs
@@ -1,7 +1,7 @@
-// import { resolve } from "https://deno.land/std@0.95.0/path/mod.ts";
 import { toFileUrl } from "https://deno.land/std@0.146.0/path/mod.ts";
 
 const firstArg = Deno.args[0];
-await import(toFileUrl(Deno.cwd()).pathname + `/${firstArg}`)
 
-// await import(resolve(Deno.cwd(), firstArg));
+await import(
+  new URL(firstArg, toFileUrl(Deno.cwd()).href + "/").href
+);
